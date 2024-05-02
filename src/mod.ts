c6749529e9dfe8956b9ac29acd3d0c3428c90ba7
Filipe Brito Ferreira <me@fbritoferreira.com>
2024-05-03 01:03:07 +0200

    ✨ Add steam API client factory
    Added a module with a factory function to create a Steam API client with
    methods to set key and output format. Also exported related types and
    constants.

--- src/mod.ts
+++ src/mod.ts
@@ -1,3 +1,7 @@
-export function hello(name: string): string {
-  return `Hello, ${name}!`;
-}
+export {
+  STEAM_API_BASE_URL,
+  type STEAM_API_OUTPUT_FORMAT,
+  type SteamApiClient,
+  type SteamApiConfig,
+  createSteamApiClient,
+} from "./client";
